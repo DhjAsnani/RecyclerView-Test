a1e3bccbf4531f395f441629666975c986c8bf20
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-12 19:02:30 +0530

    recycler view implemented

--- app/src/main/java/com/example/gohan/recyclesview/MainActivity.java
+++ app/src/main/java/com/example/gohan/recyclesview/MainActivity.java
@@ -1,24 +1,42 @@
 package com.example.gohan.recyclesview;
 
-import android.app.Application;
+import  android.app.Application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.test.ApplicationTestCase;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+    ArrayList<ListProvider> arrayList= new ArrayList<ListProvider>();
+    String[] Name,Capital;
+    int[] img_id = {R.drawable.c,R.drawable.a,R.drawable.b};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView= (RecyclerView) findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        Name = getResources().getStringArray(R.array.countryName);
+        Capital = getResources().getStringArray(R.array.capital);
+        int count = 0;
+        for(String NAME:Name)
+        {
+            ListProvider listProvider=new ListProvider(img_id[count],NAME,Capital[count]);
+                arrayList.add(listProvider);
+                count++;
+
+        }
+        adapter = new RecyclerAdapter(arrayList);
+        recyclerView.setAdapter(adapter);
     }
 
-    /**
-     * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
-     */
-    public static class ApplicationTest extends ApplicationTestCase<Application> {
-        public ApplicationTest() {
-            super(Application.class);
-        }
-    }
+
 }
--- app/src/main/java/com/example/gohan/recyclesview/RecyclerAdapter.java
+++ app/src/main/java/com/example/gohan/recyclesview/RecyclerAdapter.java
@@ -1,7 +1,65 @@
 package com.example.gohan.recyclesview;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by Freeware Sys on 6/12/2016.
  */
-public class RecyclerAdapter {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder> {
+    // declaring array list
+    ArrayList<ListProvider> arrayList = new ArrayList<ListProvider>();
+
+    public RecyclerAdapter(ArrayList<ListProvider> arrayList)
+    {
+        this.arrayList = arrayList;
+    }
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        RecyclerViewHolder recyclerViewHolder;
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_layout,parent,false);
+        recyclerViewHolder = new RecyclerViewHolder(view);
+        return recyclerViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+
+        ListProvider listProvider;
+        listProvider = arrayList.get(position);
+        holder.list_image.setImageResource(listProvider.getImage_id());
+        holder.list_country.setText(listProvider.getName());
+        holder.list_capital.setText(listProvider.getCapital());
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder
+    {
+        // for list
+        ImageView list_image;
+        TextView list_country,list_capital;
+        //for header
+        TextView title_flag,title_country,title_capital;
+        public RecyclerViewHolder(View view)
+        {
+            super(view);
+            list_image = (ImageView) view.findViewById(R.id.flag_id);
+            list_capital= (TextView) view.findViewById(R.id.capitol_id);
+            list_country= (TextView) view.findViewById(R.id.country_id);
+
+        }
+    }
 }
+
+
